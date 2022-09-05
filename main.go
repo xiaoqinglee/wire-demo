c9374ba86a905fef7d08521f73e6641f2dbd4beb
李小庆 <xiaoqinglee9@163.com>
2022-09-05 19:40:38 +0800

    test initialization success

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
 
 type Message string
 
@@ -10,10 +15,15 @@ func NewMessage() Message {
 
 type Greeter struct {
 	Message Message // <- adding a Message field
+	Grumpy  bool
 }
 
 func NewGreeter(m Message) Greeter {
-	return Greeter{Message: m}
+	var grumpy bool
+	if time.Now().Unix()%2 == 0 {
+		grumpy = true
+	}
+	return Greeter{Message: m, Grumpy: grumpy}
 }
 
 func (g Greeter) Greet() Message {
@@ -24,8 +34,11 @@ type Event struct {
 	Greeter Greeter // <- adding a Greeter field
 }
 
-func NewEvent(g Greeter) Event {
-	return Event{Greeter: g}
+func NewEvent(g Greeter) (Event, error) {
+	if g.Grumpy {
+		return Event{}, errors.New("could not create event: event greeter is grumpy")
+	}
+	return Event{Greeter: g}, nil
 }
 
 func (e Event) Start() {
@@ -42,7 +55,10 @@ func (e Event) Start() {
 //}
 
 func main() {
-	e := InitializeEvent()
-
+	e, err := InitializeEvent()
+	if err != nil {
+		fmt.Printf("failed to create event: %s\n", err)
+		os.Exit(2)
+	}
 	e.Start()
 }
--- wire.go
+++ wire.go
@@ -5,8 +5,8 @@ package main
 
 import "github.com/google/wire"
 
-func InitializeEvent() Event {
+func InitializeEvent() (Event, error) {
 	//In Wire, initializers are known as "providers," functions which provide a particular type.
 	wire.Build(NewEvent, NewGreeter, NewMessage)
-	return Event{}
+	return Event{}, nil
 }
